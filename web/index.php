eaa5daaf381bd674d83cb5cb55d2864e39c709b6
Juraj Surman <juraj@surman.sk>
2017-07-05 09:35:26 +0200

    prod

--- web/index.php
+++ web/index.php
@@ -6,9 +6,9 @@ use Symfony\Component\Debug\Debug;
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
-Debug::enable();
-//$kernel = new AppKernel('prod', false);
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
+//Debug::enable();
+//$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
